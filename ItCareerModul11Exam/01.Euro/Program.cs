5481b81c103a0adc7f031e6e3e929f3f83b5f6fe
Aleksandar Nikolov <145496882+AleksandarNiko@users.noreply.github.com>
2025-11-23 11:41:11 +0200

    Add files via upload

---
+++ ItCareerModul11Exam/01.Euro/Program.cs
@@ -0,0 +1,18 @@
+﻿
+decimal bgn = decimal.Parse(Console.ReadLine());
+
+decimal exchangeRate = decimal.Parse(Console.ReadLine());
+
+decimal commission = decimal.Parse(Console.ReadLine());
+
+if(bgn < 0 || exchangeRate < 0 || (commission < 0 || commission > 100))
+{
+    Console.WriteLine("Invalid input data!");
+    return;
+}
+
+decimal eurBeforeTax= bgn / exchangeRate;
+decimal commissionAmount = eurBeforeTax * (commission / 100);
+decimal eurAfterTax = eurBeforeTax - commissionAmount;
+
+Console.WriteLine($"Final amount: {eurAfterTax:f2} EUR");
---
+++ ItCareerModul11Exam/02.VowelLetters/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string word = Console.ReadLine();
+int vowelCount = 0;
+foreach (char letter in word)
+{
+    if ("aeiouyAEIOUY".Contains(letter))
+    {
+        vowelCount++;
+    }
+}
+if (vowelCount > 0)
+{
+    Console.WriteLine($"Vowels: {vowelCount}");
+}
+else
+{
+    Console.WriteLine("No vowels");
+}
